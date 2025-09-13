3bf390253b5e352adebb6e64fb4796a4411cbc2d
abhi-170 <abhinavabhinav66587@gmail.com>
2025-09-14 03:31:18 +0530

    import and update routes and middlewares in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,15 +2,33 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './Configs/dbConfig.js';
+import errorHandler from './Middlewares/errorHandler.js'
+
+
+// Import routes
+import authRoutes from './Routes/authRoutes.js';
+import transactionRoutes from './Routes/transactionRoutes.js';
 
 dotenv.config();
 const app= express();
 
 const PORT=process.env.PORT || 8181;
 
-app.get('/',(req,res)=>{
-    res.send(`Backend is live here`);
-})
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send(`Personal Finance Assistant Backend is live!`);
+});
+
+app.use('api/auth',authRoutes);
+app.use('/api/transactions', transactionRoutes);
+
+// Error handling middleware
+app.use(errorHandler);
 
 (async()=>{
     try {
